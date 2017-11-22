d184510c5187a086e32f2624e8c67eee65ecbf7f
Nguyen <vnguyen468@student.cccd.edu>
2017-11-21 16:18:02 -0800

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffeineListActivity.java
@@ -0,0 +1,103 @@
+package edu.orangecoastcollege.cs273.caffeinefinder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+//TODO: (1) Implement the OnMapReadCallback interface for Google Maps
+//TODO: First, you'll need to compile GoogleMaps in build.gradle
+//TODO: and add permissions and your Google Maps API key in the AndroidManifest.xml
+public class CaffeineListActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private DBHelper db;
+    private List<Location> allLocationsList;
+    private ListView locationsListView;
+    private LocationListAdapter locationListAdapter;
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caffeine_list);
+
+        deleteDatabase(DBHelper.DATABASE_NAME);
+        db = new DBHelper(this);
+        db.importLocationsFromCSV("locations.csv");
+
+        allLocationsList = db.getAllCaffeineLocations();
+        locationsListView = (ListView) findViewById(R.id.locationsListView);
+        locationListAdapter = new LocationListAdapter(this, R.layout.location_list_item, allLocationsList);
+        locationsListView.setAdapter(locationListAdapter);
+
+        //TODO: (2) Load the support map fragment asynchronously
+        //Instruct Android to load a google map into fragment (caffeineMapFragment)
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.caffeineMapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //This method is called AFTER the map is loaded from Google Play Services
+        //At this point the map is ready
+
+        //Store the reference to the google map in our member variable. (mMap)
+        mMap = googleMap;
+
+        //Custom Marker (Big Blue one - my_marker.png)
+        LatLng myPosition = new LatLng(33.671028, -117.911305);
+        // Add a custom marker at "myPosition"
+        mMap.addMarker(new MarkerOptions()
+                .position(myPosition)
+                .title("My Location")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.my_marker)));
+
+        //center the camera over myPostion(Instead of Africa)
+        CameraPosition cameraPosition= new CameraPosition.Builder().target(myPosition)
+                .zoom(15.0f)
+                .build();
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        mMap.animateCamera(cameraUpdate);
+        //mMap.moveCamera(cameraUpdate);
+
+        //plot marker for each coffee location
+        for (Location location : allLocationsList)
+        {
+            LatLng caffeineLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions()
+            .position(caffeineLocation)
+            .title(location.getName()));
+        }
+
+    }
+
+    // TODO: (3) Implement the onMapReady method, which will add a special "marker" for our current location,
+    // TODO: which is 33.671028, -117.911305  (MBCC 139)
+    // TODO: Then add normal markers for all the caffeine locations from the allLocationsList.
+    // TODO: Set the zoom level of the map to 15.0f
+
+
+    // TODO: (4) Create a viewLocationsDetails(View v) method to create a new Intent to the
+    // TODO: CaffeineDetailsActivity class, sending it the selectedLocation the user picked from the locationsListView
+    public void viewLocationDetails(View v)
+    {
+        LinearLayout selectedLayout = (LinearLayout) v;
+        Location selectedLocation = (Location) selectedLayout.getTag();
+        selectedLocation.getCity();
+    }
+}
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/Location.java
@@ -0,0 +1,175 @@
+package edu.orangecoastcollege.cs273.caffeinefinder;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * The <code>Location</code> class represents a place where one can get a caffeine fix, including
+ * its name, address, phone number and latitude/longitude location.
+ *
+ * @author Michael Paulding
+ */
+
+public class Location implements Parcelable{
+    private long mId;
+    private String mName;
+    private String mAddress;
+    private String mCity;
+    private String mState;
+    private String mZipCode;
+    private String mPhone;
+    private double mLatitude;
+    private double mLongitude;
+
+    public Location(long id, String name, String address, String city, String state, String zipCode, String phone, double latitude, double longitude) {
+        mId = id;
+        mName = name;
+        mAddress = address;
+        mCity = city;
+        mState = state;
+        mZipCode = zipCode;
+        mPhone = phone;
+        mLatitude = latitude;
+        mLongitude = longitude;
+    }
+
+    private Location(Parcel parcel)
+    {
+        mId = parcel.readLong();
+        mName = parcel.readString();
+        mAddress = parcel.readString();
+        mCity = parcel.readString();
+        mState = parcel.readString();
+        mZipCode = parcel.readString();
+        mPhone = parcel.readString();
+        mLatitude = parcel.readDouble();
+        mLongitude = parcel.readDouble();
+    }
+
+    public Location(String name, String address, String city, String state, String zipCode, String phone, double latitude, double longitude) {
+        this(-1, name, address, city, state, zipCode, phone, latitude, longitude);
+    }
+
+    public long getId() {
+        return mId;
+    }
+
+    public void setId(long id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getAddress() {
+        return mAddress;
+    }
+
+    public void setAddress(String address) {
+        mAddress = address;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public void setCity(String city) {
+        mCity = city;
+    }
+
+    public String getState() {
+        return mState;
+    }
+
+    public void setState(String state) {
+        mState = state;
+    }
+
+    public String getZipCode() {
+        return mZipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        mZipCode = zipCode;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public void setPhone(String phone) {
+        mPhone = phone;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(double latitude) {
+        mLatitude = latitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(double longitude) {
+        mLongitude = longitude;
+    }
+
+    public String getFullAddress()
+    {
+        return mAddress + "\n" + mCity + ", " + mState + "  " + mZipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "Id=" + mId +
+                ", Name='" + mName + '\'' +
+                ", Address='" + mAddress + '\'' +
+                ", City='" + mCity + '\'' +
+                ", State='" + mState + '\'' +
+                ", Zip Code='" + mZipCode + '\'' +
+                ", Phone='" + mPhone + '\'' +
+                ", Latitude=" + mLatitude +
+                ", Longitude=" + mLongitude +
+                '}';
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeLong(mId);
+        parcel.writeString(mName);
+        parcel.writeString(mAddress);
+        parcel.writeString(mCity);
+        parcel.writeString(mState);
+        parcel.writeString(mZipCode);
+        parcel.writeString(mPhone);
+        parcel.writeDouble(mLatitude);
+        parcel.writeDouble(mLongitude);
+    }
+
+    public static final Parcelable.Creator<Location> CREATOR = new Creator<Location>() {
+        @Override
+        public Location createFromParcel(Parcel parcel) {
+            return new Location(parcel);
+        }
+
+        @Override
+        public Location[] newArray(int i) {
+            return new Location[i];
+        }
+    };
+}
